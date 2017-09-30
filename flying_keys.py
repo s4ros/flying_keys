2b3d5adbefdb133e30b53a562e930e3f726993ae
s4ros <s4ros@infinigy.pl>
2017-09-30 17:53:58 +0200

    First version of smoke imitation..

--- flying_keys.py
+++ flying_keys.py
@@ -4,6 +4,7 @@ import pygame
 import queue
 import threading
 import random
+import os
 
 
 class Hooker(object):
@@ -26,20 +27,19 @@ class ThreadKeyHooker(threading.Thread):
 
 
 class Particle(object):
-    def __init__(self, startx, starty, col, width):
+    def __init__(self, startx, starty, width):
         self.x = startx
         self.y = starty
         self.wide = width
-        self.color = col
 
     def move(self, x, y):
-        self.x = x + random.randint(0, self.wide)
-        self.y = y + random.randint(-5, 15)
-        self.color -= 2
+        self.x = x
+        self.y = y
 
 
 class Keystroke(object):
-    def __init__(self, surface, font):
+    def __init__(self, surface, font, images):
+        self.images = images
         max_x, max_y = surface.get_size()
         self.x = 10
         self.dx = random.randint(3, 25)
@@ -48,43 +48,44 @@ class Keystroke(object):
         self.gravity = 0.3
         self.width = font.get_width() + 4
         self.height = font.get_height()
-        self.color = 255
+        # self.color = 255
         self.font = font
-        self.n_particles = 50
-        self.particles = [Particle(self.x, self.y, 255, self.width)
+        self.n_particles = 1
+        self.particles = [Particle(self.x - (self.width),
+                                   self.y - (self.height),
+                                   self.width)
                           for x in range(self.n_particles)]
 
     def draw(self, surface):
-        pygame.draw.rect(
-            surface,
-            (self.color, 0, 0),
-            (self.x, self.y, self.width, self.height),
-            0)
+        # pygame.draw.rect(
+        #     surface,
+        #     (self.color, 0, 0),
+        #     (self.x, self.y, self.width, self.height),
+        #     0)
         for p in self.particles:
-            pygame.draw.circle(
-                surface,
-                (p.color, p.color, 0),
-                (int(p.x), int(p.y)), 2
-            )
-            p.move(self.x,
-                   self.y - self.dy)
+            p.move(self.x - self.width ,
+                   self.y - self.dy - self.height // 2)
+            surface.blit(random.choice(images), (p.x, p.y))
         surface.blit(self.font, (self.x + 2, self.y))
 
     def newton(self):
         self.dy += self.gravity
         self.x += self.dx
         self.y += self.dy
-        self.color -= 2
+        # self.color -= 2
 
 
 class KeyVisualizer(object):
-    def __init__(self, q, width=640, height=480):
+    def __init__(self, q, images, width=640, height=480):
         self.queue = q
+        self.images = images
         self.width = width
         self.height = height
+        # keyboard thread
         self.tkh = ThreadKeyHooker(self.queue)
         self.tkh.setDaemon(True)
         self.tkh.start()
+        # //
         pygame.init()
         self.clock = pygame.time.Clock()
         self.font = pygame.font.Font(None, 42)
@@ -107,7 +108,7 @@ class KeyVisualizer(object):
 
     def create_button(self, key):
         text = self.font.render(key, True, (255, 255, 255))
-        g = Keystroke(self.screen, text)
+        g = Keystroke(self.screen, text, self.images)
         self.buttons.append(g)
 
     def draw_all_buttons(self):
@@ -130,8 +131,12 @@ class KeyVisualizer(object):
             self.clock.tick(30)
 
 
-
 if __name__ == "__main__":
     q = queue.Queue()
-    kv = KeyVisualizer(q, 1720, 1080)
+    images = []
+    for file in os.listdir("PNG/black_smoke"):
+        if file.endswith(".png"):
+            filepath = os.path.join("PNG/black_smoke", file)
+            images.append(pygame.image.load(filepath))
+    kv = KeyVisualizer(q, images, 1720, 1080)
     kv.run()
